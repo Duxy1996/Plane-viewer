1a49e5e49370503257b9c19902038bdea051f7db
duxy1996 <durancdr96@gmail.com>
2018-02-11 21:40:29 +0100

    Added basic kml parser

---
+++ js/index.js
@@ -0,0 +1,120 @@
+var sceneEl = document.querySelector('a-scene');
+
+var flight = "";
+
+function up_down(event) {
+  sceneEl = document.querySelector('a-scene');
+  camera = sceneEl.querySelector('#cam');
+  z = camera.components.position.attrValue.z;
+  y = camera.components.position.attrValue.y;
+  x = camera.components.position.attrValue.x;
+  var let = event.which || event.keyCode;
+  if (let == 101){
+    camera.setAttribute('position', {x: x, y: (y+0.05), z: z});
+  }
+  if (let == 113){
+    camera.setAttribute('position', {x: x, y: (y-0.05), z: z});
+  }
+
+}
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                flight = allText;
+                flight = get_data(flight)
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+readTextFile("file:///C:/Users/Carlos/Documents/GitHub/Plane_viewer/flight/6W703-106596d0.kml");
+
+function get_data(flight){
+  flight = flight.replace(/\//g,"");
+  flight = flight.replace(/<Point>/g,"");
+  flight = flight.replace(/<Placemark>/g,"");
+  flight = flight.replace(/<altitudeMode>absolute<altitudeMode>/g,"");
+  flight = flight.replace(/<altitudeMode>clampToGround<altitudeMode>/g,"");
+  flight = flight.replace(/<TimeStamp>/g,"");
+  flight = flight.replace(/<Style>/g,"");
+  flight = flight.replace(/<width>2<width>/g,"");
+  flight = flight.replace(/<tessellate>1<tessellate>/g,"");
+  flight = flight.replace(/<IconStyle>/g,"");
+  flight = flight.replace(/<LineStyle>/g,"");
+  flight = flight.replace(/<Folder>/g,"");
+  flight = flight.replace(/<width>/g,"");
+  flight = flight.replace(/<MultiGeometry>/g,"");
+  flight = flight.replace(/<LineString>/g,"");
+  flight = flight.replace(/<tessellate>/g,"");
+  flight = flight.replace(/<MultiGeometry>/g,"");
+  flight = flight.replace(/<Icon>/g,"");
+  flight = flight.replace(/<div>/g,"");
+  flight = flight.replace(/<span>/g,"");
+  flight = flight.replace(/<b>/g,"");
+  flight = flight.replace(/Altitude:/g,"");
+  flight = flight.replace(/Speed:/g,"");
+  flight = flight.replace(/ft/g,"");
+  flight = flight.replace(/kt/g,"");
+  flight = flight.replace(/&deg/g,"");
+  flight = flight.replace(/Heading:/g,"");
+  flight = flight.replace(/<when>/g,"");
+  flight = flight.replace(/<heading>/g,"");
+  flight = flight.replace(/<href>/g,"");
+  flight = flight.replace(/Trail/g,"");
+  flight = flight.replace(/<heading>87<heading>/g,"");
+  flight = flight.replace(/ffffffff/g,"");
+  flight = flight.replace(/<coordinates>/g,"");
+  flight = flight.replace(/<name>/g,"");
+  flight = flight.split("![CDATA[");
+  var i = 0;
+  tramited_flight = [];
+  for(i = 0; i < flight.length; i++){
+    test = flight[i];
+    test = test.replace(/ /g,"");
+    test = test.replace(/<description></g,"");
+    test = test.replace(/]]><description>/g,"");
+    test = test.replace(/f*/g,"");
+    test = test.split("\n");
+    test = test.filter(function(a){return a !== ""});
+    if(test.length == 8) {
+      tramited_flight.push(test);
+    }
+  }
+  return tramited_flight;
+}
+
+console.log(sceneEl);
+
+
+function add_sphere(x,y,z){
+  sceneEl = document.querySelector('a-scene');
+  var sphere = document.createElement('a-sphere');
+  sphere.setAttribute('geometry', {
+    radius:1
+  });
+  sphere.setAttribute('position', {x: x, y: y, z: z});
+  sphere.setAttribute('material', 'color', '#C6B566');
+  sphere.setAttribute('shadow','cast','true');
+  sceneEl.appendChild(sphere);
+}
+
+setTimeout(
+  function(){
+    for(i = 0; i < flight.length; i++){
+      pos = flight[i][6].split(",");
+      alt = flight[i][0].replace(/,/g,"");
+      add_sphere((pos[0]-37)*800 - 800,alt/200,(pos[1]-55)*800 -300);
+    }
+
+}
+,500)
